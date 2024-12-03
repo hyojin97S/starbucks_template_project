c3f55238c2ff283f1292c475b0e631b1602057c1
hyojin97S <gywls7948@naver.com>
2024-12-03 15:54:00 +0900

    Starbucks Korea Site

---
+++ js/common.js
@@ -0,0 +1,19 @@
+// search
+const searchEl = document.querySelector('.search');
+const searchInputEl = document.querySelector('input');
+
+searchEl.addEventListener('click', () => {
+  searchInputEl.focus();
+});
+searchInputEl.addEventListener('focus', (e) => {
+  searchEl.classList.add('focused');
+  e.target.setAttribute('placeholder','통합검색');
+});
+searchInputEl.addEventListener('blur', (e) => {
+  searchEl.classList.remove('focused');
+  e.target.setAttribute('placeholder','');
+});
+
+// footer year
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
